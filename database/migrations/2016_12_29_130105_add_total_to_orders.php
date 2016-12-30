63175f33367a5624c6a96380b254ddb0bbbe4d3d
phucduye53 <phucduye53@gmail.com>
2016-12-30 10:46:39 +0700

    add a column for order table

---
+++ database/migrations/2016_12_29_130105_add_total_to_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTotalToOrders extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('orders',function($table){
+			$table->integer('total');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
