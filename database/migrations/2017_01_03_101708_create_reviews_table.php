2fca73fc4e81ca58f1dd21c3ec532ebde4467c36
phucduye53 <phucduye53@gmail.com>
2017-01-03 18:41:15 +0700

    add review table

---
+++ database/migrations/2017_01_03_101708_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('reviews', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('id_product');
+			$table->string('text');
+			$table->string('username');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('reviews');
+	}
+
+}
