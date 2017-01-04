227d90084f8d88605dc0b9fec8a29b30de9d26f8
mypower119 <doit.programer@gmail.com>
2017-01-04 21:38:08 +0700

    Fix error for UI : alert-danger replace by alert-success

--- resources/views/admin/cate/list.blade.php
+++ resources/views/admin/cate/list.blade.php
@@ -12,7 +12,7 @@
                     @include('admin.blocks.error')
                     <div class="col-lg-12">
                       @if(Session::has('flash_message'))
-                      <div class="alert alert-danger">
+                      <div class="alert alert-success">
                         {!! Session::get('flash_message') !!}
                       </div>
                       @endif
